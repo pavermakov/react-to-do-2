b21e18a2bb513f52375413c985910c4563e8c885
Pavel Ermakov <pavermakov@gmail.com>
2016-10-24 18:24:59 +0300

    Testing AddTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  handleFormSubmit: function(e){
+    e.preventDefault();
+    var todoText = this.refs.todoText.value;
+
+    if(todoText.length > 0){
+      this.refs.todoText.value = '';
+      this.props.onAddTodo(todoText);
+    } else {
+      this.refs.todoText.focus();
+    }
+  },
+  render: function(){
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <input type="text" ref="todoText" placeholder="Enter a new task" />
+          <button className="button expanded">Add todo</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = AddTodo;
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
   getInitialState: function(){
@@ -20,12 +21,16 @@ var TodoApp = React.createClass({
       ]
     }
   },
+  handleAddTodo: function(text){
+    alert('New todo added: ' + text);
+  },
   render: function(){
     var {todos} = this.state;
 
     return (
       <div>
         <TodoList todos={todos} />
+        <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
     );
   }
