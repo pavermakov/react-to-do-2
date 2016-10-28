41bdaa3a98f2a3bd943be59343b3d20510c1a25f
Pavel Ermakov <pavermakov@gmail.com>
2016-10-28 13:48:41 +0300

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -20,5 +20,33 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompleted, searchText){
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter(todo => {
+      return !todo.completed || showCompleted;
+    });
+
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter(todo => {
+      var text = todo.text.toLowerCase();
+      return text.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((prevTodo, nextTodo) => {
+      // return -1 if prevTodo comes before nextTodo
+      if(!prevTodo.completed && nextTodo.completed){
+        return -1;
+      } else if(prevTodo.completed && !nextTodo.completed){
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -42,6 +42,48 @@ describe('TodoAPI', () => {
       var actualTodos = TodoAPI.getTodos();
 
       expect(actualTodos).toEqual(todos);
-    })
+    });
+  });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'Test 1',
+      completed: false
+    },{
+      id: 2,
+      text: 'Some Test 2',
+      completed: true
+    },{
+      id: 3,
+      text: 'Test 3',
+      completed: false
+    }];
+
+    it('should return all items if showCompleted set to true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return only completed items when showCompleted is set to false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+      expect(filteredTodos[1].completed).toBe(false);
+    });
+
+    it('should filter todos by search text', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should return all todos if search text is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
   });
 });
